e9ab1438cffb86e1def96106f2d39f1d9e208449
Joel <35474476+JoelMite@users.noreply.github.com>
2023-03-22 12:56:21 -0500

    Styles Final 1

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> --}}
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> 
   <title>Sistema de Agendamiento de Citas Medicas</title>
   <!-- General CSS Files -->
   {{-- <link rel="stylesheet" href="assets/css/app.min.css">
